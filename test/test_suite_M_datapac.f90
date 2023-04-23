674e653f8f2c22ced5e4e9f2b2974b0b1b2e0bc5
John S. Urban <urbanjost@comcast.net>
2023-04-23 18:03:54 -0400

    testing

--- test/test_suite_M_datapac.f90
+++ test/test_suite_M_datapac.f90
@@ -6,8 +6,7 @@ use, intrinsic :: ISO_FORTRAN_ENV, only : INT8, INT16, INT32, INT64       !  1  
 use, intrinsic :: ISO_FORTRAN_ENV, only : REAL32, REAL64, REAL128         !  4           8          10
 use M_framework__msg
 use M_framework__verify, only : unit_check_start,unit_check,unit_check_done,unit_check_good,unit_check_bad,unit_check_msg
-use M_framework__verify, only : unit_check_level
-use M_framework__verify, only : unit_check_command, unit_check_keep_going, unit_check_level, unit_check_stop
+use M_framework__verify, only : unit_check_stop
 use M_datapac
 !use M_test_suite_M_anything
 !use M_anything, only : anyinteger_to_string, anyscalar_to_int64
@@ -17,9 +16,6 @@ use M_datapac
 
 implicit none
 !! setup
-   unit_check_command=''
-   unit_check_keep_going=.true.
-   unit_check_level=0
 !! test
    call test_autoco()
    call test_betran()
